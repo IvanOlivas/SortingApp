a6737c446792e03d524608e12b208f5cdbf9ad59
Ivan Olivas <ivanolivas@Ivans-MacBook-Pro.local>
2021-03-25 00:35:10 -0400

    edited syntax for reading names, and edited if/elif statements

--- sortList.py
+++ sortList.py
@@ -1,21 +1,22 @@
 
 def sort_list():
     sortIt = list();                #create list to store strings
-    doc = open('Sort Me.txt');      #access the file with the string
+    doc = open("Sort Me.txt", "r");      #access the file with the string
 
     #read through file and store each string
     for name in doc:
         sortIt.append(name);
     doc.close();           # close file since all strings were stored in our list
 
-    #sortIt.sort()  #sort by the length of the string
-   
-    sortIt.sort(key=lambda s: (len(s), s))
-    
     #ask user if they want to reverse the sort
     val = input("Would you like to reverse the sort? (enter 1 for yes, 2 for no):")
     
-    if val == 1:
+    if val == 1:   # final result in reverse
         sortIt.sort(reverse=True)
+        sortIt.sort(key=len, reverse=True)
+        
+    elif val == 2:  #not reversed
+        sortIt.sort()
+        sortIt.sort(key=len)
 
     return sortIt;
